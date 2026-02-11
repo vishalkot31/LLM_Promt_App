e7d01cdd7a8cd10fa16e899ff4bea6f5235cef7f
“Vishal <vishal.chikki@gmail.com>
2026-02-11 18:22:17 +0530

    Initial Commit

---
+++ LLM_Promt_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LLM_Promt_App
+//
+//  Created by Vishal Kothari on 11/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LLM_Promt_App/LLM_Promt_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLM_Promt_AppApp.swift
+//  LLM_Promt_App
+//
+//  Created by Vishal Kothari on 11/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LLM_Promt_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
